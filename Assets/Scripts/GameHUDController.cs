674ecf21a189a2a0f1eefdb61b9684e856e1028a
Emin Buğra Aksoy <120595513+NUREMDER@users.noreply.github.com>
2025-11-29 14:43:42 +0300

    Health Bar added
    Health-bar added for DungeonHandler Character

---
+++ Assets/Scripts/GameHUDController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UIElements; 
+
+public class GameHUDController : MonoBehaviour
+{
+    private ProgressBar healthBar; 
+    private Stats playerStats;     
+
+    void OnEnable()
+    {
+        
+        var uiDocument = GetComponent<UIDocument>();
+        if (uiDocument == null) return;
+
+        var root = uiDocument.rootVisualElement;
+        healthBar = root.Q<ProgressBar>("health-bar");
+
+        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            playerStats = player.GetComponent<Stats>();
+
+            if (playerStats != null)
+            {
+                
+                playerStats.OnHealthChanged += UpdateHealthUI;
+
+               
+                UpdateHealthUI(playerStats.getHp(), playerStats.getMaxHp());
+            }
+        }
+        else
+        {
+            Debug.LogWarning("GameHUDController: 'Player' tag'ine sahip nesne bulunamadý!");
+        }
+    }
+
+    void OnDisable()
+    {
+        
+        if (playerStats != null)
+        {
+            playerStats.OnHealthChanged -= UpdateHealthUI;
+        }
+    }
+
+    
+    void UpdateHealthUI(float currentHp, float maxHp)
+    {
+        if (healthBar != null)
+        {
+            
+            healthBar.highValue = maxHp;
+            
+            healthBar.value = currentHp;
+        }
+    }
+}
--- Assets/Scripts/Stats/Stats.cs
+++ Assets/Scripts/Stats/Stats.cs
@@ -1,10 +1,9 @@
-using System;
+using System; 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Stats : MonoBehaviour
 {
-
     [SerializeField] private float maxHp;
     [SerializeField] private float hp;
     [SerializeField] private float armor = 0;
@@ -13,24 +12,43 @@ public class Stats : MonoBehaviour
     [SerializeField] private string visualName;
     private Animator animator;
 
+    
+    public event Action<float, float> OnHealthChanged;
+
     private void Awake()
     {
         hp = maxHp;
-        animator = transform.Find(visualName).GetComponent<Animator>();
+        
+        if (!string.IsNullOrEmpty(visualName))
+        {
+            Transform visualTransform = transform.Find(visualName);
+            if (visualTransform != null)
+                animator = visualTransform.GetComponent<Animator>();
+        }
     }
+
     
-    
+    private void Start()
+    {
+        OnHealthChanged?.Invoke(hp, maxHp);
+    }
+
     public void TakeDamage(float amount)
     {
         float damage = amount - armor;
-        if(damage < 0) {damage = 0;}
+        if (damage < 0) { damage = 0; }
+
         hp -= damage;
-        if (hp <= 0) {Die();}
+
+        
+        OnHealthChanged?.Invoke(hp, maxHp);
+
+        if (hp <= 0) { Die(); }
     }
 
     public void Die()
     {
-        animator.SetBool("die", true);
+        if (animator != null) animator.SetBool("die", true);
     }
 
     public void OnDestroy()
@@ -41,29 +59,10 @@ public class Stats : MonoBehaviour
         }
     }
 
-    public float getAttackDamage()
-    {
-        return attackDamage;
-    }
-
-    public float getMaxHp()
-    {
-        return maxHp;
-    }
-
-    public float getArmor()
-    {
-        return armor;
-    }
-
-    public float getCooldownReduction()
-    {
-        return cooldownReduction;
-    }
-
-    public float getHp()
-    {
-        return hp;
-    }
-
+    
+    public float getAttackDamage() { return attackDamage; }
+    public float getMaxHp() { return maxHp; }
+    public float getArmor() { return armor; }
+    public float getCooldownReduction() { return cooldownReduction; }
+    public float getHp() { return hp; }
 }
